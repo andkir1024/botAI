ad3b3877cfe7f6fc910858f0ec0e1e567ad339c4
andkir1024 <andkir@mail.ru>
2023-07-18 04:06:29 +0300

    fix users

--- processorMenu.py
+++ processorMenu.py
@@ -5,6 +5,7 @@ from jsoncomment import JsonComment
 
 # import jsoncomment
 from aiogram import Bot, Dispatcher, executor, types
+from userDB import *
 
 class processorMenu:
     def __init__(self, name):
@@ -12,8 +13,14 @@ class processorMenu:
         parser = JsonComment(json)
         with open(name, 'r', encoding='utf-8') as f: #открыли файл с данными
             self.parsed_object = parser.load(f)
+            
+        # user = userDB(True)
+        # user.getUserInfo(1000)
         return
     async def doMenu(self, message):
+        user = userDB(True)
+        user.getUserInfo(message.chat.id)
+        await message.reply(str(message.chat.id))
         await message.reply("Привет!\nЯ Эхо-бот\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
         
         return
---
+++ userDB.py
@@ -0,0 +1,6 @@
+class userDB:
+    def __init__(self, isModel):
+        self.isModel = isModel
+        return
+    def getUserInfo(self, id):
+        return None
