77c744a718eb4e64df5d7637e14aef3e055f2ad2
kirpichnikov.a@insitech.ru <kirpichnikov.a@insitech.ru>
2023-08-02 13:34:52 +0300

    update

--- kbs.py
+++ kbs.py
@@ -1,7 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-kb_clients = ReplyKeyboardMarkup()
-b1 = KeyboardButton('say 1')
-b2 = KeyboardButton('say 2')
-b3 = KeyboardButton('say 3')
-kb_clients.add(b1).add(b2).add(b3)
+def get_kb() -> ReplyKeyboardMarkup:
+    kb_clients = ReplyKeyboardMarkup()
+    b1 = KeyboardButton('say 1')
+    b2 = KeyboardButton('say 2')
+    b3 = KeyboardButton('say 3')
+    kb_clients.add(b1).add(b2).add(b3)
+    return kb_clients
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from processorMenu import *
-from kbs import kb_clients
+from kbs import get_kb
+# from kbs import get_kb, kb_clients
 # API_TOKEN = '6232440369:AAG-bS18nYh-cXVUWrMKham3mP6OTjTaw4k'
  
 bot = Bot(token=mainConst.API_TOKEN)
@@ -9,7 +10,8 @@ menu = processorMenu("config_ru.jsonc")
  
 @dp.message_handler(commands=['start'])
 async def cmd_start(msg: types.Message) -> None:
-    await msg.answer('Text', reply_markup=kb_clients)
+    await msg.answer('Text', reply_markup=get_kb())
+   #  await msg.answer('Text', reply_markup=kb_clients)
 
 
 @dp.message_handler(commands=['cancel'])
