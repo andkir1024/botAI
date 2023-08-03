13294615d002c45e2fcd09d3c7c7927a88f80832
kirpichnikov.a@insitech.ru <kirpichnikov.a@insitech.ru>
2023-08-03 17:31:12 +0300

    гзвфеу

--- okDeskUtils.py
+++ okDeskUtils.py
@@ -1,18 +1,17 @@
 
 import requests
+# from requests import async
 from commonData import mainConst
-
 
 class okDesk:
     # поиск оборудования по InvetoryId
     def findEquipmentByInvetoryId(inventory_number):
-        # Поиск оборудования
         URL = 'https://insitech.okdesk.ru/api/v1/equipments/?' + mainConst.OKDESK_TOKEN + '&inventory_number=' + str(inventory_number)
         res = requests.get(URL)
-        # res = requests.post(URL, data={'st3': 'jim hopper'})
-        # res = requests.post('https://httpbin.org/post', data={'st3': 'jim hopper'})
-        # print(res.text)
         return
 
-        # $result = botUtils::getOkDesk($sURL);
-		# return $dataRes;
+    # поиск точки обслуживания по maintenance_entity_id
+    def findMaintenanceEntitiesByInvetoryId(maintenance_entity_id):
+        URL = 'https://insitech.okdesk.ru/api/v1/maintenance_entities/' + str(maintenance_entity_id) + '?' + mainConst.OKDESK_TOKEN
+        res = requests.get(URL)
+        return
